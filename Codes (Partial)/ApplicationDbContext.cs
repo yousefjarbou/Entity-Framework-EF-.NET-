1d3bbf804a08e5bae57da9d957594ee90c8b54ec
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2025-02-02 06:59:35 +0300

    Add files via upload

---
+++ Codes (Partial)/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thirdEntityFramworkTry
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=YOUSEFPC\SQLEXPRESS; Initial Catalog=thirdEntityFramworkTry12Aug; Integrated Security=True; TrustServerCertificate=True;");
+            //@"Data Source=YOUSEFPC\SQLEXPRESS; Initial Catalog=master;Integrated Security=True;"
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
